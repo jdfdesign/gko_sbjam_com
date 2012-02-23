dadccf50e80f9fc2f02621fc909cc264e14f975d
jdfdesign <regis@joufdesign.com>
2012-02-23 11:45:19 -0430

    bump

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -1,13 +1,20 @@
-//= require gko/externals/jquery.elastidegallery
-
-
-jQuery(document).ready(function ($) {
+//= require gko/jquery.elastidegallery 
+	
+$(document).ready(function() {
 	if($('.images:first').length > 0) {
 		Gallery.init($('.images:first'));
 	}
+  
+	$("div.custom.dropdown").each(function () {
+    $(this).css('width', '260px').find('ul').css('width', '260px');
+  });
+	
 	$('#featured').orbit({ 
-		bullets : false, 
-		animation : "fade",
+		bullets : true, 
+		animation : "horizontal-push",
 		animationSpeed: 800,
-		timer: true });
+		timer: true,
+		fluid: false });
+		
+	$('.block-grid .entry-thumb').prepend("<div class='overlay'>+</div>");
 });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120127133051) do
+ActiveRecord::Schema.define(:version => 20120223150851) do
 
   create_table "accounts", :force => true do |t|
     t.string   "reference",  :limit => 40
